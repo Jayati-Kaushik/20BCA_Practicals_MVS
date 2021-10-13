14dc5ec8ab4fc5959dfc763d51b584632af9620a
Firas <91874069+316firas@users.noreply.github.com>
2021-10-13 22:53:37 +0300

    Add files via upload

---
+++ 73_Firas2.py
@@ -0,0 +1,65 @@
+import numpy as np
+from numpy import linalg
+
+#6D vector
+v = np.array([[1,2,3,4,5,6]])
+print(v)
+print("\n")
+
+#transpose
+print(v.T)
+print("\n")
+
+#2 non-square matrix that can be multiplied
+m1=np.array([[1,0,2],[0,1,3]])
+m2=np.array([[1,2],[2,1],[3,4]])
+
+#matrix shape
+print(m1.shape)
+print(m2.shape)
+print("\n")
+
+# matrix product
+print(np.matmul(m1,m2))
+print("\n")
+
+# sum: two non square matrices of same order
+x=np.array([[1,2,1],[2,1,2]])
+y=np.array([[1,1,1],[2,2,3]])
+print(x+y)
+print("\n")
+
+# Define a square matrix A.
+a=np.matrix([[1,2],[3,4]])
+print(a)
+print("\n")
+
+# Print the identity matrix of the above order I.
+i=np.array([[1,0],[0,1]])
+print(i)
+print("\n")
+
+# Verify A.I = I.A for matrix multiplication.
+print("A.I= ",a@i)
+print("I.A= ",i@a)
+print("A.I = I.A")
+
+# Define another square matrix of the same order as A
+a1=np.array([[4,5],[6,7]])
+print("\n")
+
+# Print the product of the matrices as matrix multiplication
+print(a@a1)
+print("\n")
+
+# Print the product of the matrices by element wise multiplication
+print(np.multiply(a,a1))
+print("\n")
+
+# Calculate and print the inverse of A. (Use linalg)Check if determinant is 0 Use if else statement to calculate inverse only when determinant isnon 0
+d=np.linalg.det(a)
+print("Determinnt: ",d)
+if d!=0:
+    print("Inverse: ", np.linalg.inv(a))
+else:
+    print("Inverse does not exist")
