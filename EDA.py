27cfef2bf558a593802531a4160992305a05a23c
Khushi <71957748+kxushi@users.noreply.github.com>
2021-10-27 14:14:50 +0530

    uploaded lab 3 codes
    plotted inbuilt iris dataset

---
+++ EDA.py
@@ -0,0 +1,25 @@
+#basic EDA
+import pandas as pd
+import numpy as np
+#import os
+import seaborn as sns
+import matplotlib.pyplot as plt
+
+#os.chdir("path")
+iris = pd.read_csv('Iris.csv')
+print(iris.head())
+print(iris.describe())
+sns.countplot(x='Species', data=iris)
+sns.scattterplot('SepalLengthCm','SepalWidthCm', hue='Species',data=iris)
+sns.pairplot(iris.drop(['Id'], axis =1),hue='Species',height=2)
+#sns.boxenplot()
+x=iris.corr(method='pearson')
+print(x)
+sns.heatmap(iris.corr_matrix,method='pearson'.drop(['Id'],axis=1).drop(['Id'],axis=0))
+plt.boxplot('SepalWidthCm', data=iris)
+
+plt.show()
+
+#cd python3 bin
+#python -n pip install seaborn
+#pip install seaborn
