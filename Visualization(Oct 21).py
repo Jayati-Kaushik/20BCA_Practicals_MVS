c157ae09a36d8f15930edb77a6ac0d6b65f49514
christoroshan <91861931+christoroshan@users.noreply.github.com>
2021-10-28 23:59:13 +0530

    Add files via upload

---
+++ Visualization(Oct 21).py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Oct 28 23:08:16 2021
+
+@author: chris
+"""
+
+import pandas as pd
+import numpy as np
+import os
+import seaborn as sns
+import matplotlib.pyplot as plt
+os.chdir("C:/Users/chris/Desktop/Jk Python Prac")
+iris = pd.read_csv('Iris.csv')
+iris.head()
+print(iris.head())
+print(iris.describe())
+sns.countplot(x='class', data = iris)
+plt.show()
+sns.scatterplot('SepalLength','SepalWidth', hue='class',data = iris)
+plt.show()
+sns.pairplot(iris.drop(['Id'],axis =1), hue= 'class', height= 2)
+plt.show()
+sns.boxenplot()
+plt.show()
+sns.heatmap(iris.corr(), data = iris)
+plt.show()
+x= iris.corr(method= 'pearson')
+print(x)
+
+
+
