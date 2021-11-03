978b1bf34019298ede18040ba6f18a4ef9483cc1
Dhanya <91933170+Dhanyaravi25@users.noreply.github.com>
2021-11-03 18:20:42 +0530

    Add files via upload

---
+++ 20bcab10.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+"""
+
+
+@author: DHANYA
+"""
+
+import pandas as pd
+import numpy as np
+import os
+import seaborn as sns
+import matplotlib.pyplot as plt
+os.chdir("C:/Users/DHANYA/Desktop/dhanya")
+iris = pd.read_csv('Iris.csv')
+iris.head()
+print(iris.head())
+print(iris.describe())
+sns.countplot(x='Species', data = iris)
+plt.show()
+sns.scatterplot('SepalLengthCm','SepalWidthCm', hue='Species',data = iris)
+plt.show()
+sns.pairplot(iris.drop(['Id'],axis =1), hue= 'Species', height= 2)
+plt.show()
+sns.heatmap(iris.corr(), data = iris)
+plt.show()
+x= iris.corr(method= 'pearson')
+print(x)
