93f68d33838385a936881cf3e667e33662bf6d0e
sanjayks2023 <91878483+sanjayks2023@users.noreply.github.com>
2021-11-14 20:14:09 +0530

    Add files via upload

---
+++ visualisation(oct21) updated.py
@@ -0,0 +1,22 @@
+import pandas as pd
+import numpy as np
+import os
+import seaborn as sns
+import matplotlib.pyplot as plt
+os.chdir("C:/Users/sanjay/Desktop/Jk Python Prac")
+iris = pd.read_csv('Iris.csv')
+iris.head()
+print(iris.head())
+print(iris.describe())
+sns.countplot(x='Species', data = iris)
+plt.show()
+sns.scatterplot('SepalLengthCm','SepalWidthCm', hue='Species',data = iris)
+plt.show()
+sns.pairplot(iris.drop(['Id'],axis =1), hue= 'Species', height= 2)
+plt.show()
+sns.boxenplot()
+plt.show()
+sns.heatmap(iris.corr(), data = iris)
+plt.show()
+x= iris.corr(method= 'pearson')
+print(x)
