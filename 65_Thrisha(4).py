49b0c870b55abc4e952f2e37e1c8a7da7f3c1790
Thrisha <91831109+Thrisha12@users.noreply.github.com>
2021-12-06 22:38:46 +0530

    Add files via upload

---
+++ 65_Thrisha(4).py
@@ -0,0 +1,56 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Nov  4 15:40:04 2021
+
+@author: DEC BEAST
+"""
+
+import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
+from sklearn import linear_model
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import mean_squared_error, r2_score
+
+best = pd.read_csv('bestsellers.csv')
+print(best.head())
+print(best.describe())
+sns.countplot(x =  'User Rating', data= best)
+plt.show()
+sns.scatterplot(x='Price' ,y='Reviews', data= best)
+plt.show()
+sns.scatterplot(x='Genre',y='Reviews', data= best)
+plt.show()
+sns.pairplot(best.drop(['Name'], axis=1) ,height=2)
+plt.show()
+A = best.corr(method='pearson')
+print(A)
+sns.heatmap(best.corr(method='pearson').drop(['Year'],axis=1).drop(['Year'],axis=0))
+plt.show()
+plt.boxplot('User Rating', data= best)
+plt.show()
+
+#regression for one variable
+X= best[['User Rating']]
+Y = best['Price']
+X_train, X_test, Y_train, Y_test = train_test_split(X,Y,test_size=0.3)
+reg = linear_model.LinearRegression()
+reg.fit(X_train,Y_train)
+Y_pred = reg.predict(X_test)
+print("Coefficients: \n", reg.coef_)
+print("Mean squared error: %.2f" % mean_squared_error(Y_test, Y_pred))
+print("Coefficient of determination: %.2f" % r2_score(Y_test, Y_pred))
+print("Variance score: {}".format(reg.score(X_test,Y_test)))
+sns.regplot(X,Y)
+
+#Multiple regrssion
+X = best[['User Rating','Reviews','Year']]
+Y = best['Price']
+X_train, X_test, Y_train, Y_test = train_test_split(X,Y,test_size=0.3)
+reg = linear_model.LinearRegression()
+reg.fit(X_train,Y_train)
+Y_pred = reg.predict(X_test)
+print("Coefficients: \n", reg.coef_)
+print("Mean squared error: %.2f" % mean_squared_error(Y_test, Y_pred))
+print("Coefficient of determination: %.2f" % r2_score(Y_test, Y_pred))
+print("Variance score: {}".format(reg.score(X_test,Y_test)))
