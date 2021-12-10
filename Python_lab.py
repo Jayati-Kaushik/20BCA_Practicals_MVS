e39b2c7866a95b1f8dee47b29da4d746ca41093f
krishjeremy <72058185+krishjeremy@users.noreply.github.com>
2021-12-10 19:04:08 +0530

    Add files via upload

---
+++ Python_lab.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Dec 10 18:50:46 2021
+
+@author: ACER
+"""
+
+import pandas as pd
+import numpy as np
+import os
+import seaborn as sns
+import matplotlib.pyplot as plt
+
+os.chdir("C:/Users/ACER/Downloads")
+iris = pd.read_csv('Iris.csv')
+print(iris.head())
+print(iris.describe())
+sns.countplot(x='Species', data = iris)
+plt.show()
+sns.scatterplot('SepalLengthCm','SepalWidthCm', hue='Species',data = iris)
+plt.show()
+sns.pairplot(iris.drop(['Id'],axis =1), hue= 'Species', height= 2)
+plt.show()
+sns.heatmap(iris.corr(), data = iris)
+plt.show()
+x= iris.corr(method= 'pearson')
+print(x)
+sns.heatmap(iris.corr(method='pearson').drop(['Id'],axis=1).drop(['Id'],axis=0))
