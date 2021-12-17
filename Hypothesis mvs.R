8bcf4c6bee997e09cd60d879fba7b7e467371ad5
Aarushi-25 <91716748+Aarushi-25@users.noreply.github.com>
2021-12-17 19:32:44 +0530

    Update Hypothesis mvs.R

--- Hypothesis mvs.R
+++ Hypothesis mvs.R
@@ -4,6 +4,6 @@ car=read.csv("CarPrice_Assignment.csv")
 View(car)
 #Ho:mean of enginesize =120
 #H1:mean of enginesize is not equal to 120
-mean(data$enginesize) #mean = 126.8971
-t.test(data$enginesize,mu=120,alternative="less",conf.level = 0.95)
+mean(car$enginesize) #mean = 126.8971
+t.test(car$enginesize,mu=120,alternative="less",conf.level = 0.95)
 #we get p = 0.9908
