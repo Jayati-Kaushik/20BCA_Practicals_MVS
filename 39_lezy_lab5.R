6130d15eb9851babfa97c8c0642e51100f7a2bde
Lezy daniel <91894403+Lezy-daniel@users.noreply.github.com>
2021-12-22 22:13:31 +0530

    Add files via upload

---
+++ 39_lezy_lab5.R
@@ -0,0 +1,9 @@
+getwd()
+setwd("C:/Users/lezy daniel/OneDrive/Desktop/daniel2")
+car1=read.csv("CarPrice_Assignment-1.csv")
+View(car1)
+mean(car1$carwidth)
+#mean = 65.9078
+#Ho: mean of carwidth is equal to 65 
+#H1: mean of carwidth is not equal to 65
+t.test(car1$carwidth,mu=65,alternative = "less",conf.level = 0.95)
