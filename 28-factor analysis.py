56c1437f1d1770dbc2cf58ee7ae861d6bd75a975
NAVEEN38160 <72302643+NAVEEN38160@users.noreply.github.com>
2022-01-07 17:40:54 +0530

    Add files via upload

---
+++ 28-factor analysis.py
@@ -0,0 +1,48 @@
+import numpy as np
+import pandas as pd
+import seaborn as sns
+from sklearn.datasets import load_iris
+from factor_analyzer import FactorAnalyzer
+
+
+import matplotlib.pyplot as plt
+import os
+
+os.chdir("C:/Users/naveen/Downloads")
+df= pd.read_csv('bikes_price.csv')
+df.info()
+df.drop(['age'],axis=1,inplace=True)
+df.info()
+
+
+#to calculate bartlett sphericity
+from factor_analyzer.factor_analyzer import calculate_bartlett_sphericity
+chi_square_value,p_value=calculate_bartlett_sphericity(df)
+chi_square_value, p_value
+
+
+#to calculate Kaiser-Meyer-Olkin (KMO) Test
+from factor_analyzer.factor_analyzer import calculate_kmo
+kmo_all,kmo_model=calculate_kmo(df)
+kmo_model
+
+# Create factor analysis object and perform factor analysis
+fa = FactorAnalyzer()
+fa.analyze(df, 25, rotation=None)
+# Check Eigenvalues
+ev, v = fa.get_eigenvalues()
+ev
+
+
+# Create factor analysis object and perform factor analysis
+fa = FactorAnalyzer()
+fa.analyze(df, 6, rotation="varimax")
+fa.loadings
+
+# Create factor analysis object and perform factor analysis using 5 factors
+fa = FactorAnalyzer()
+fa.analyze(df, 5, rotation="varimax")
+fa.loadings
+
+# Get variance of each factors
+fa.get_factor_variance()
