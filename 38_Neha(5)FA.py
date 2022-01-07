addc7a10aabcbf06508f23e7061cb2787f3cf9b2
NehaC10 <91934769+NehaC10@users.noreply.github.com>
2022-01-07 21:11:20 +0530

    Add files via upload
    Factor Analysis uploaded.

---
+++ 38_Neha(5)FA.py
@@ -0,0 +1,20 @@
+import os
+import numpy as np
+import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
+from sklearn.decomposition import FactorAnalysis
+from sklearn import preprocessing
+from sklearn.datasets import load_digits
+os.chdir("C:/Users/Santosh/Desktop/Neha/Datasets")
+data=pd.read_csv('heart.csv')
+data.drop(['Age'],axis=1,inplace=True)
+print(data.head())
+print(data.dtypes)
+print(data.shape)
+print(data.head(15))
+data.info()
+X, _ = load_digits(return_X_y=True)
+fa_Analysis = FactorAnalysis(n_components=5, random_state=92)
+X_fa_Analysis  = fa_Analysis.fit_transform(X)
+print(X_fa_Analysis.shape)
