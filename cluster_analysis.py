d7e457319e3068b3f8fd80fb23d82c6026fedf5c
christoroshan <91861931+christoroshan@users.noreply.github.com>
2022-01-31 23:33:29 +0530

    Add files via upload

---
+++ cluster_analysis.py
@@ -0,0 +1,36 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Jan 31 23:05:09 2022
+
+@author: chris
+"""
+
+from sklearn.datasets import load_iris
+from sklearn.cluster import AgglomerativeClustering
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+from scipy.cluster.hierarchy import dendrogram, linkage 
+import os
+
+os.chdir("C:/Users/chris/Desktop/20BCAB23")
+dataset = pd.read_csv("CarPrice_Assignment.csv")
+dataset.drop(['car_ID','CarName'],axis=1,inplace=True)
+dataset.info()
+df = dataset.iloc[:, [8,9]].values
+
+Z = linkage(df, method = "ward")
+dendro = dendrogram(Z)
+plt.title('Dendogram')
+plt.ylabel('Euclidean distance')
+plt.show()
+ac = AgglomerativeClustering(n_clusters=4, affinity="euclidean", linkage="ward")
+
+labels = ac.fit_predict(df)
+plt.figure(figsize = (8,5))
+plt.scatter(df[labels == 0,0] , df[labels == 0,1], c= 'purple')
+plt.scatter(df[labels == 1,0] , df[labels == 1,1], c= 'blue')
+plt.scatter(df[labels == 2,0] , df[labels == 2,1], c= 'silver')
+plt.scatter(df[labels == 3,0] , df[labels == 3,1], c= 'black')
+plt.scatter(df[labels == 4,0] , df[labels == 4,1], c= 'orange')
+plt.show()
