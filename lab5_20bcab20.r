85e62aa007c43968223054c7df49ed176238b469
adithyakrishnan-kk <91901847+adithyakrishnan-kk@users.noreply.github.com>
2022-03-19 20:25:42 +0530

    Delete lab5_20bcab20.r

--- lab5_20bcab20.r
+++
@@ -1,9 +0,0 @@
-getwd()
-setwd("C:/Users/ADITHYA KRISHNAN/OneDrive/Desktop")
-car1=read.csv("CarPrice_Assignment-1.csv")
-View(car1)
-mean(car1$carwidth)
-#mean = 65.9078
-#Ho: mean of carwidth is equal to 65 
-#H1: mean of carwidth is not equal to 65
-t.test(car1$carwidth,mu=65,alternative = "less",conf.level = 0.95)
