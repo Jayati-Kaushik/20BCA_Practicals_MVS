33a4084c27fbf2c41f92b6c6522f19e0df7d9158
shahid13-khan <91713811+shahid13-khan@users.noreply.github.com>
2022-03-28 22:08:05 +0530

    Add files via upload

---
+++ FA_20BCAB18.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Mar 28 21:58:18 2022
+
+@author: Shahid
+"""
+
+from sklearn.datasets import load_digits
+from sklearn.decomposition import FactorAnalysis
+import os
+import pandas as pd
+import numpy as np
+
+os.chdir("C:/Users/Shahid Khan/OneDrive/Desktop/Shahid")
+d1 = pd.read_csv("covid_19_india.csv")
+print(d1.head())
+print(d1.dtypes)
+print(d1.info())
+X, _ = load_digits(return_X_y=True)
+fa_Analysis = FactorAnalysis(n_components=8, random_state=123)
+X_fa_Analysis  = fa_Analysis.fit_transform(X)
+print(X_fa_Analysis.shape)
