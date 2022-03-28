7271f28cf54637a7f33c48c8a3549902eb3c34da
Prajwalkumar95 <91900410+Prajwalkumar95@users.noreply.github.com>
2022-03-28 21:22:10 +0530

    Delete 17:12:21 (20BCAB54).R

--- 17:12:21 (20BCAB54).R
+++
@@ -1,8 +0,0 @@
-setwd("/Users/yuvraj/Desktop")
-data=read.csv(file.choose())
-#if p value is less than alpha (significantvalue)(alpha=1-confidence level), we reject null hypothesis 
-#H0
-mean(data$carlength)
-t.test(data$carlength,mu=120,conf.level = 0.95)
-
-t.test(data$carlength,mu=120,alternative = "less",conf.level = 0.95)
