8e74bc7364785dc384f1928f22b07d309f725f04
leosamuelgilbert <91947536+leosamuelgilbert@users.noreply.github.com>
2022-03-28 18:14:43 +0530

    Add files via upload

---
+++ 20bcab48_leo_stats.py
@@ -0,0 +1,35 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Mar 28 18:10:50 2022
+
+@author: hp
+"""
+
+import numpy as np
+X=np.array([[1,2,3],[3,2,1],[9,10,7]])
+print(X)
+Y=np.array([[1,2,3],[3,2,1],[9,10,7]]).T
+print(X)
+A=np.array([[0]*3])
+print(A)
+print(np.zeros((4,8)))
+print(np.ones(4))
+print(X)
+print(Y)
+print(X+Y)
+print(np.shape(X))
+I=np.array([[10,20,30,40,50,60,70]])
+print(I)
+print(I[0,1],I[0,3],I[0,6])
+print(np.shape(I))
+print(I.T)
+V=np.array([[1,2,3,4,5],[6,7,8,9,10],[11,12,13,14,15],[16,17,18,19,20]])
+print(V)
+print(np.shape(V))
+print(V.T)
+print(V[:,0])
+print(V[0,:])
+print(np.zeros(7))
+print(np.ones(3))
+
+
---
+++ 20bcab48_leo_statsiris.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Mar 28 18:05:40 2022
+
+@author: hp
+"""
+
+import pandas as pd
+import numpy as np
+import os
+import seaborn as sns
+import matplotlib.pyplot as plt
+
+os.chdir("C:/Users/hp/Downloads")
+iris = pd.read_csv('Iris.csv')
+print(iris.head())
+print(iris.describe())
+sns.countplot(x='Species', data = iris)
+plt.show()
+#sns.scatterplot('SepalLengthCm','SepalWidthCm', hue='Species',data = iris)
+#plt.show()
+#sns.pairplot(iris.drop(['Id'],axis =1), hue= 'Species', height= 2)
+#plt.show()
+#sns.heatmap(iris.corr(), data = iris)
+#plt.show()
+#x= iris.corr(method= 'pearson')
+#print(x)
+#sns.heatmap(iris.corr(method='pearson').drop(['Id'],axis=1).drop(['Id'],axis=0))
