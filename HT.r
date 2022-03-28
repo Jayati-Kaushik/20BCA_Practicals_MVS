7e39dcd5ad5752a002f15092067243cc822ee0c9
Mijolan <86546168+mijolan@users.noreply.github.com>
2022-03-28 16:02:08 +0530

    Create HT.r

---
+++ HT.r
@@ -0,0 +1,7 @@
+getwd()
+setwd("C:/mijolan/documents/python")
+data=read.csv("CarPrice_Assignment.csv")
+mean(data$curbweight)
+# H0: Mean curbweight = 2550
+# H1: Mean curbweight > 2550
+t.test(data$curbweight,mu=2550,alternative ='two.sided',conf.level=0.95)
